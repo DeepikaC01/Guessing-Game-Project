afcb3056b4e7bfac4c3f90ee602680c44dfdddc1
Deepika C <93979075+DeepikaC01@users.noreply.github.com>
2021-11-10 12:07:40 +0530

    Add files via upload

---
+++ Guessing Game Project.py
@@ -0,0 +1,10 @@
+# Secret_Number = 9
+# Guess_Count = 0
+# while Guess_Count <3:
+#     Guess = int(input('Guess: '))
+#     Guess_Count = Guess_Count + 1
+#     if Guess == Secret_Number:
+#         print('You won')
+#         break
+# else:
+#     print('Sorry you have guessed all wrong')
